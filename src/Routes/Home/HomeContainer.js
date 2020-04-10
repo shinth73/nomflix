7b62c770e46bb8dd639a4178f8f56e98b15252e4
shinth73 <60809725+shinth73@users.noreply.github.com>
2020-04-10 19:23:31 +0900

    20200410

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -8,24 +8,25 @@ export default class extends React.Component {
     upcoming: null,
     popular: null,
     error: null,
-    loading: true
+    loading: true,
   };
 
   async componentDidMount() {
     try {
       const {
-        data: { results: nowPlaying }
+        data: { results: nowPlaying },
       } = await moviesApi.nowPlaying();
+      console.log(nowPlaying);
       const {
-        data: { results: upcoming }
+        data: { results: upcoming },
       } = await moviesApi.upcoming();
       const {
-        data: { results: popular }
+        data: { results: popular },
       } = await moviesApi.popular();
       this.setState({
         nowPlaying,
         upcoming,
-        popular
+        popular,
       });
     } catch {
       this.setState({ error: "Cant't find movies information" });
